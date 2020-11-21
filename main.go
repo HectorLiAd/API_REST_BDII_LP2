634689db437defc4d8167861536ac06fa8e5a4df
HectorLiAd <hectorlimahuaya@upeu.edu.pe>
2020-11-21 17:57:33 -0500

    3

--- main.go
+++ main.go
@@ -6,18 +6,25 @@ import (
 
 	// "github.com/API_REST_BDII_LP2/database"
 
+	"github.com/HectorLiAd/API_REST_BDII_LP2/database"
 	"github.com/HectorLiAd/API_REST_BDII_LP2/handler"
+	"github.com/HectorLiAd/API_REST_BDII_LP2/persona"
 	"github.com/go-chi/chi"
 )
 
 func main() {
-	// db := database.InitDB()
-	// defer db.Close()
+	db := database.InitDB()
+	defer db.Close()
+
+	var personaRepository = persona.NewRepository(db)
+	var personaServicio = persona.NerService(personaRepository)
+
 	r := chi.NewRouter()
 	r.Use(handler.GetCors().Handler)
-	r.Get("/persona", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome personita 1"))
-	})
+
+	r.Mount("/persona", persona.MakeHTTPHandler(personaServicio))
+
+	// dandole un puerto segun lo que requiera heroku
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
